21fd69275a22913e7ccc67c7ab32ad4b6c24ad48
Haya <hayachannel5@gmail.com>
2024-07-04 21:20:11 +0900

    init: Functions側のコードを作成し、CDを設定

---
+++ api/get_info/__init__.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+import azure.cosmos as cosmos
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    COSMOS_CONNECTION_STRING = os.environ["COSMOS_CONNECTION_STRING"]
+    COSMOS_DATABASE_NAME = os.environ["COSMOS_DATABASE_NAME"]
+    COSMOS_CONTAINER_NAME = os.environ["COSMOS_CONTAINER_NAME"]
+
+    cosmos_client = cosmos.CosmosClient.from_connection_string(COSMOS_CONNECTION_STRING)
+    cosmos_database = cosmos_client.get_database_client(COSMOS_DATABASE_NAME)
+    cosmos_container = cosmos_database.get_container_client(COSMOS_CONTAINER_NAME)
+
+    item = cosmos_container.read_item(item="protocol", partition_key="protocol")
+
+    body_dict = {}
+    body_dict["room_temperature"] = item["room_temperature"]
+    body_dict["cpu_temperature"] = item["cpu_temperature"]
+    body_json = json.dumps(body_dict)
+
+    return func.HttpResponse(body=body_json, status_code=200, mimetype="application/json")
