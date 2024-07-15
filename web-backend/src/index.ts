769ea8eab68df3477dca7f4b3a36bb8360ffa638
Haya <hayachannel5@gmail.com>
2024-07-15 09:03:43 +0900

    feature: サーバーの起動時間を返すように変更

--- web-backend/src/index.ts
+++ web-backend/src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000; // App Serviceでは、自動的にポート番号が割り当てられる
+const serverStartedUnixTime = Date.now();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world");
+  var serverRunningTime = Date.now() - serverStartedUnixTime;
+  res.send(`${serverRunningTime}ms`);
 });
 
 app.listen(port, () => {
